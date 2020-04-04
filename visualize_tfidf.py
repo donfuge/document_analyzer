8960f5f9565e7067ed5ad2a4ce846e96e7087d76
fuge <fulop.gergo@gmail.com>
2020-04-04 17:49:51 +0200

    improved plotting

--- visualize_tfidf.py
+++ visualize_tfidf.py
@@ -15,7 +15,7 @@ https://stackoverflow.com/questions/27494202/how-do-i-visualize-data-points-of-t
 
 """
 
-# standard imports
+#%% standard imports
 import os
 import sys
 import pickle
@@ -26,6 +26,9 @@ from utils import safe_pickle_dump, strip_version, Config
 
 
 import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib.patches as mpatches
+
 import seaborn as sns
 sns.set()
 from sklearn.cluster import KMeans
@@ -41,7 +44,7 @@ def update_annot(ind):
                            " ".join([str(names[n]) for n in ind["ind"]]))
     annot.set_text(text)
     #annot.get_bbox_patch().set_facecolor(cmap(norm(c[ind["ind"][0]])))
-    annot.get_bbox_patch().set_alpha(0.4)
+    annot.get_bbox_patch().set_alpha(0.9)
 
 
 def hover(event):
@@ -58,7 +61,7 @@ def hover(event):
                 fig.canvas.draw_idle()
 
 
-#%%
+#%% Load previously compiled data and run PCA, t-SNE
 
 
 # load the tfidf matrix and meta
@@ -74,14 +77,22 @@ names = meta['fnames']
 
 
 pca_num_components = 2
+tsne_num_components = 2
+
 
 reduced_data = PCA(n_components=pca_num_components).fit_transform(X)
+embeddings = TSNE(n_components=tsne_num_components)
 
-#%% scatterplot, coloring based on vocabulary/bigram
+Y_tsne = embeddings.fit_transform(X)
+#%% PCA and t-SNE scatterplot, coloring based on vocabulary/bigram (makes plot
+# and saves it)
 
 vocab = meta['vocab']
 
-bigrams = ['quantum','majorana','qubit','machine learning']
+bigrams = ['quantum','josephson','qubit','machine learning','graphene','nanowire','microwave',
+            'noise', 'quantum dot', 'electronics', 'quality factor']
+
+# bigrams = ['quantum']
 
 for bigram in bigrams:
 
@@ -92,67 +103,120 @@ for bigram in bigrams:
     print('bigram not found in vocabulary: %s' % bigram)
     break
   
-  #threshold = 0.01
-  threshold = np.mean(X[:,bigram_id])
+  threshold = 0.01
+  # threshold = np.mean(X[:,bigram_id])
   #threshold = np.quantile(X[:,bigram_id],0.7)
   
   true_document_idx = np.asarray(X[:,bigram_id] >= threshold)
   true_document_idx = true_document_idx.reshape(-1).T
   
+  cmap = cm.get_cmap('coolwarm')
+  
+  fig, ax = plt.subplots(1,2,figsize=(12,5))
+  sc = ax[0].scatter(reduced_data[:,0], reduced_data[:,1],c=true_document_idx,cmap=cmap)
+
+  # sc = ax[0].scatter(reduced_data[true_document_idx,0], reduced_data[true_document_idx,1],color='g', label = bigram)
+  # sc = ax[0].scatter(reduced_data[~true_document_idx,0], reduced_data[~true_document_idx,1],color='b', label = "not "+ bigram)
+  true_patch = mpatches.Patch(color=cmap(cmap.N), label=bigram)
+  false_patch = mpatches.Patch(color=cmap(0), label="not " + bigram)
+
+  ax[0].legend(handles=[true_patch, false_patch])
+  
+  ax[0].set_title("PCA, n-gram: %s" % bigram)
+  ax[0].set_xlabel('Principal axis 1')
+  ax[0].set_ylabel('Principal axis 2')
+  
+
+
+  sc = ax[1].scatter(Y_tsne[:,0], Y_tsne[:,1],c=true_document_idx,cmap=cmap)
+
+  ax[1].legend(handles=[true_patch, false_patch])
+  # sc = ax[1].scatter(Y_tsne[true_document_idx,0], Y_tsne[true_document_idx,1],color='g', label = bigram)
+  # sc = ax[1].scatter(Y_tsne[~true_document_idx,0], Y_tsne[~true_document_idx,1],color='b', label = "not "+ bigram)
+  ax[1].legend(handles=[true_patch, false_patch])
+  
+  ax[1].set_title("t-SNE, n-gram: %s" % bigram)
+
+  ax[1].set_xlabel('Embedding axis 1')
+  ax[1].set_ylabel('Embedding axis 2')
+  
+  plt.show()
+  plt.savefig("plots/" + bigram + ".png")
+
+#%% interactive plot, shows filename when hovering (PCA)
+bigram = 'microwave'
+
+if bigram in vocab:
+  bigram_id = vocab[bigram]
+  print('id of %s : %d' % (bigram, bigram_id ))
+  
+  threshold = np.mean(X[:,bigram_id])
+  
+  true_document_idx = np.asarray(X[:,bigram_id] >= threshold)
+  
+  cmap = cm.get_cmap('coolwarm')
+ 
   
   fig, ax = plt.subplots()
+  sc = ax.scatter(reduced_data[:,0], reduced_data[:,1],c=true_document_idx,cmap=cmap)
   
-  sc = ax.scatter(reduced_data[true_document_idx,0], reduced_data[true_document_idx,1],color='g', label = bigram)
-  sc = ax.scatter(reduced_data[~true_document_idx,0], reduced_data[~true_document_idx,1],color='b', label = "not "+ bigram)
+  true_patch = mpatches.Patch(color=cmap(cmap.N), label=bigram)
+  false_patch = mpatches.Patch(color=cmap(0), label="not " + bigram)
+
+  ax.legend(handles=[true_patch, false_patch])
   
+  annot = ax.annotate("", xy=(-200,0), xytext=(-200,20),textcoords="offset points",
+                      bbox=dict(boxstyle="round", fc="w"),
+                      arrowprops=dict(arrowstyle="->"))
+  annot.set_visible(False)
+  
+  fig.canvas.mpl_connect("motion_notify_event", hover)
+  
+  plt.title("PCA")
   plt.xlabel('Principal axis 1')
   plt.ylabel('Principal axis 2')
-  
-  plt.title("PCA, bigram: %s" % bigram)
-  plt.legend()
   plt.show()
-  plt.savefig("PCA_" + bigram + ".png")
+else:
+  print('bigram not found in vocabulary: %s' % bigram)
+  
+  
 
-#%% interactive plot, show filename when hovering
+#%% interactive plot, shows filename when hovering (t-SNE)
+bigram = 'machine learning'
 
-# print reduced_data
+if bigram in vocab:
+  bigram_id = vocab[bigram]
+  print('id of %s : %d' % (bigram, bigram_id ))
+  
+  threshold = np.mean(X[:,bigram_id])
+  
+  true_document_idx = np.asarray(X[:,bigram_id] >= threshold)
+  
+  cmap = cm.get_cmap('coolwarm')
+ 
+  
+  fig, ax = plt.subplots()
+  sc = ax.scatter(Y_tsne[:,0], Y_tsne[:,1],c=true_document_idx,cmap=cmap)
+  
+  true_patch = mpatches.Patch(color=cmap(cmap.N), label=bigram)
+  false_patch = mpatches.Patch(color=cmap(0), label="not " + bigram)
 
-fig, ax = plt.subplots()
-sc = ax.scatter(reduced_data[:,0], reduced_data[:,1])
-plt.title("PCA")
+  ax.legend(handles=[true_patch, false_patch])
+  
+  annot = ax.annotate("", xy=(-200,0), xytext=(-200,20),textcoords="offset points",
+                      bbox=dict(boxstyle="round", fc="w"),
+                      arrowprops=dict(arrowstyle="->"))
+  annot.set_visible(False)
+  
+  fig.canvas.mpl_connect("motion_notify_event", hover)
+  
+  ax.set_title("t-SNE, n-gram: %s" % bigram)
 
-annot = ax.annotate("", xy=(-200,0), xytext=(-200,20),textcoords="offset points",
-                    bbox=dict(boxstyle="round", fc="w"),
-                    arrowprops=dict(arrowstyle="->"))
-annot.set_visible(False)
-
-fig.canvas.mpl_connect("motion_notify_event", hover)
-
-plt.xlabel('Principal axis 1')
-plt.ylabel('Principal axis 2')
-plt.show()
-#%%
-
-tsne_num_components = 2
-
-
-# t-SNE plot
-embeddings = TSNE(n_components=tsne_num_components)
-Y = embeddings.fit_transform(X)
-
-fig, ax = plt.subplots()
-sc = plt.scatter(Y[:, 0], Y[:, 1], cmap=plt.cm.Spectral)
-plt.title("t-SNE")
-
-annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
-                    bbox=dict(boxstyle="round", fc="w"),
-                    arrowprops=dict(arrowstyle="->"))
-annot.set_visible(False)
-
-fig.canvas.mpl_connect("motion_notify_event", hover)
-
-plt.show()
-
+  ax.set_xlabel('Embedding axis 1')
+  ax.set_ylabel('Embedding axis 2')
+  plt.show()
+else:
+  print('bigram not found in vocabulary: %s' % bigram)
 
 
 
